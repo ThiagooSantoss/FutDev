cac5124b0253dc11e87c5ce614ad444232796fa8
Thiago Santos <thiagosantosmunhoz@gmail.com>
2024-05-20 19:16:17 -0300

    teste

--- src/components/EquipeCard.tsx
+++ src/components/EquipeCard.tsx
@@ -1,6 +1,7 @@
 import { Equipe } from "@/types/equipe";
 import { converteDataFull } from "@/utils/converteDataFull";
 import Image from "next/image";
+import { useState } from "react";
 
 interface EquipeCardProps {
   equipe: Equipe;
@@ -8,6 +9,7 @@ interface EquipeCardProps {
 
 export const EquipeCard = (props: EquipeCardProps) => {
   const { equipe } = props;
+  const [virado, setVirado] = useState(false);
 
   const { cores, estadio, foto, fundacao, nome } = equipe;
 
@@ -33,29 +35,38 @@ export const EquipeCard = (props: EquipeCardProps) => {
 
   const dataFormatada = converteDataFull(fundacao);
 
+  const handleClick = () => {
+    setVirado(!virado); // Alternar entre virado e não virado quando clicado
+  };
+
   return (
     <li
       className={`bg-opacity-50 max-w-sm rounded overflow-hidden shadow-lg
          hover:shadow-zinc-400 hover:shadow-xl cursor-pointer transition-all h-fit
-      `}
+         ${virado ? 'transform rotate-y-180' : ''}`}
+      onClick={handleClick} // Adiciona o manipulador de evento de clique
     >
       <div className="p-8" style={retornaCoresCadastradas(cores)}>
-        <Image
-          className="w-full"
-          src={`http://127.0.0.1:8000/static/game/media/uploads/${foto}`}
-          alt={`foto do escudo`}
-          width={300}
-          height={300}
-        />
-      </div>
-
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{nome}</div>
-        <p className="text-gray-700 text-base mb-2">
-          Fundado em: {dataFormatada}
-        </p>
-        <p className="text-gray-700 text-base mb-2">Estádio: {estadio}</p>
+        <div className="flex justify-center items-center h-full">
+          {virado ? (
+            <div>
+              <p className="font-bold text-xl mb-2">{nome}</p>
+              <p className="text-gray-700 text-base mb-2">Fundado em: {dataFormatada}</p>
+              <p className="text-gray-700 text-base mb-2">Estádio: {estadio}</p>
+            </div>
+          ) : (
+            <Image
+              className="w-full"
+              src={`http://127.0.0.1:8000/static/game/media/uploads/${foto}`}
+              alt={`foto do escudo`}
+              width={300}
+              height={300}
+            />
+          )}
+        </div>
       </div>
     </li>
   );
 };
+
+
