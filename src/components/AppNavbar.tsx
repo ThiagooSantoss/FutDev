7cc76a16f55da9e6f8e51478fb7fd2ccd3118959
Cristiano de Paula <crdepa@gmail.com>
2024-07-05 21:26:44 -0300

    adição de novo logo e correção no burger menu

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -9,19 +9,19 @@ import Link from "next/link"
 
 export const AppNavbar = () => {
   return (
-    <Navbar className="bg-[#5b7e28] items-end" fluid rounded menuOpen>
+    <Navbar className="bg-[#5b7e28] items-end">
       <Navbar.Brand as={Link} href="https://flowbite-react.com">
         <Image
-          src={"/topo.jpg"}
+          src={"/logo.png"}
           alt={"logo"}
-          width={350}
-          height={350}
+          width={200}
+          height={200}
         />
       </Navbar.Brand>
-
-      <Navbar.Collapse className="w-fit p-8">
-        <Navbar.Link href="#" className="text-white hover:text-black">Início</Navbar.Link>
-        <Navbar.Link href="/sobre" className="text-white hover:text-black">Sobre</Navbar.Link>
+      <Navbar.Toggle className="[&:not(:active)]:text-white [&:not(:active)]:bg-transparent focus:bg-[#5b7e28] active:text-white" />
+      <Navbar.Collapse>
+        <Navbar.Link href="#" className="text-white hover:text-black" active>Início</Navbar.Link>
+        <Navbar.Link as={Link} href="/sobre" className="text-white hover:text-black">Sobre</Navbar.Link>
         <Navbar.Link href="/equipes" className="text-white hover:text-black">Equipes</Navbar.Link>
         <Navbar.Link href="/jogadores" className="text-white hover:text-black">Jogadores</Navbar.Link>
         <Navbar.Link href="/contato" className="text-white hover:text-black">Contato</Navbar.Link>
