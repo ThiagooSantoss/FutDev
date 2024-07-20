31f1f99e1295a6e2542b273b6403f091d89e04fc
Matheus Pimentel - Andela <matheus.pimentel@andela.com>
2024-07-19 21:06:07 -0300

    arrumado o fetchWrapper para lidar com errors

--- src/utils/fetchWrapper.ts
+++ src/utils/fetchWrapper.ts
@@ -1,7 +1,18 @@
+interface FetchResponseInfo {
+  status: number;
+  statusText: string;
+  url: string;
+}
+
+interface FetchResponse<T> {
+  data: T;
+  responseInfo: FetchResponseInfo;
+}
+
 export async function fetchWrapper<T = unknown>(
   input: RequestInfo | URL,
   init?: RequestInit
-): Promise<T> {
+): Promise<FetchResponse<T>> {
   const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${input}`, {
     headers: {
       Authorization: "Token 1064b3e9cd8c8b4a60a105eb3180858d7de1dd6f",
@@ -10,7 +21,23 @@ export async function fetchWrapper<T = unknown>(
     ...init,
   });
 
-  const result = await data.json();
+  const responseInfo = {
+    status: data.status,
+    statusText: data.statusText,
+    url: data.url,
+  };
 
-  return result as T;
+  if (!data.ok) {
+    return {
+      data: null as T,
+      responseInfo,
+    };
+  }
+
+  const result: T = await data.json();
+
+  return {
+    data: result,
+    responseInfo,
+  };
 }
