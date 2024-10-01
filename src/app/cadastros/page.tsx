61b066f396db0e5cca5526b3b058996539e4b602
infinityfile <diego05.pimentel@gmail.com>
2024-09-30 21:29:59 -0300

    adicao de equipes na aba de cadastro

--- src/app/cadastros/page.tsx
+++ src/app/cadastros/page.tsx
@@ -1,13 +1,18 @@
-"use client";
-
 import { DefaultTable } from "@/components/DefaultTable";
 import { SelectMenu } from "@/components/SelectMenu";
+import { fetchWrapper } from "@/utils/fetchWrapper";
+import { Equipe } from "@/types/equipe";
 
-export default function Cadastros() {
+export default async function Cadastros() {
+  const response = await fetchWrapper<Equipe[]>("equipes");
+  console.log(response.data);
+
+  const equipes = response.data;
+
   return (
     <>
       <SelectMenu />
-      <DefaultTable />
+      <DefaultTable equipes={equipes} />
     </>
   );
 }
--- src/components/DefaultTable.tsx
+++ src/components/DefaultTable.tsx
@@ -1,71 +1,74 @@
 "use client";
 
+import { Equipe } from "@/types/equipe";
 import { Table } from "flowbite-react";
+import Image from "next/image";
+import { converteDataFull } from "@/utils/converteDataFull";
 
-export function DefaultTable() {
+interface ContainerEquipesProps {
+  equipes: Equipe[];
+}
+
+export const DefaultTable: React.FC<ContainerEquipesProps> = ({ equipes }) => {
   return (
     <div className="overflow-x-auto">
       <Table>
         <Table.Head>
-          <Table.HeadCell>Product name</Table.HeadCell>
-          <Table.HeadCell>Color</Table.HeadCell>
-          <Table.HeadCell>Category</Table.HeadCell>
-          <Table.HeadCell>Price</Table.HeadCell>
+          <Table.HeadCell>Escudo</Table.HeadCell>
+          <Table.HeadCell>Equipe</Table.HeadCell>
+          <Table.HeadCell>Cores</Table.HeadCell>
+          <Table.HeadCell>Fundação</Table.HeadCell>
           <Table.HeadCell>
             <span className="sr-only">Edit</span>
           </Table.HeadCell>
         </Table.Head>
         <Table.Body className="divide-y">
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {'Apple MacBook Pro 17"'}
-            </Table.Cell>
-            <Table.Cell>Sliver</Table.Cell>
-            <Table.Cell>Laptop</Table.Cell>
-            <Table.Cell>$2999</Table.Cell>
-            <Table.Cell>
-              <a
-                href="#"
-                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Edit
-              </a>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Microsoft Surface Pro
-            </Table.Cell>
-            <Table.Cell>White</Table.Cell>
-            <Table.Cell>Laptop PC</Table.Cell>
-            <Table.Cell>$1999</Table.Cell>
-            <Table.Cell>
-              <a
-                href="#"
-                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Edit
-              </a>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Magic Mouse 2
-            </Table.Cell>
-            <Table.Cell>Black</Table.Cell>
-            <Table.Cell>Accessories</Table.Cell>
-            <Table.Cell>$99</Table.Cell>
-            <Table.Cell>
-              <a
-                href="#"
-                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Edit
-              </a>
-            </Table.Cell>
-          </Table.Row>
+          {equipes?.map((equipe) => (
+            <Table.Row
+              key={equipe.id}
+              className="bg-white dark:border-gray-700 dark:bg-gray-800"
+            >
+              <Table.Cell>
+                <Image
+                  src={`http:127.0.0.1:8000/static/game/media/uploads/${equipe.foto}`}
+                  alt={`foto do escudo`}
+                  width={55}
+                  height={55}
+                />
+              </Table.Cell>
+
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {equipe.nome}
+              </Table.Cell>
+
+              <Table.Cell className="flex">
+                {equipe.cores.map((cor) => (
+                  <div
+                    key={cor}
+                    style={{
+                      backgroundColor: cor,
+                      width: "25px",
+                      height: "25px",
+                      border: "1px solid #000",
+                      borderRadius: "50%",
+                    }}
+                  ></div>
+                ))}
+              </Table.Cell>
+
+              <Table.Cell>{converteDataFull(equipe.fundacao)}</Table.Cell>
+              <Table.Cell>
+                <a
+                  href="#"
+                  className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  Edit
+                </a>
+              </Table.Cell>
+            </Table.Row>
+          ))}
         </Table.Body>
       </Table>
     </div>
   );
-}
+};
